6a66d25a6cd93256103a37318ef3faab6d240098
Somkiat Jaidee <somkiatjaidee@Somkiats-MacBook-Air.local>
2022-04-21 15:43:46 +0700

    initial

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 // client.js
-const express = require('express');
+/* const express = require('express');
 const compression = require('compression');
 const path = require('path');
 const app = express();
@@ -13,6 +13,27 @@ app.get('*', function(req, res) {
 
 const port = process.env.PORT || 3000;
 
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+}); */
+
+
+const express = require('express');
+const cors = require('cors')
+const path = require('path');
+const bodyParser = require('body-parser')
+const app = express();
+app.use(cors());
+app.use(bodyParser.json())
+
+const root = require('path').join(__dirname, 'build')
+app.use(express.static(root));
+
+app.use('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
 });
